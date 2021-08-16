685acc75adc1de87116e6a720f904262925ba0b6
Henrique Dantas <paulo.henrique@sapienstec.com>
2021-08-16 19:36:07 -0300

    [Update] Finalizando desafio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,6 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
-
 interface Image {
   title: string;
   description: string;
@@ -17,61 +16,59 @@ interface Image {
   id: string;
 }
 
-interface GetImagesParamns {
+interface GetImagesResponse {
   after: string;
   data: Image[];
 }
 
-
 export default function Home(): JSX.Element {
-
-  async function fetchImages ({pageParam = null}): Promise<GetImagesParamns> {
-    const { data } = await api('/api/images' , {
+  async function fetchImages({ pageParam = null }): Promise<GetImagesResponse> {
+    const { data } = await api('/api/images', {
       params: {
-        after: pageParam
-      }
-    })
-    return data
+        after: pageParam,
+      },
+    });
+    return data;
   }
-
 
   const {
     data,
     isLoading,
-    isError, 
+    isError,
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images', fetchImages,
-    {
-      getNextPageParam: lastPage => lastPage?.after || null,
-    }
-  );
+  } = useInfiniteQuery('images', fetchImages, {
+    getNextPageParam: lastPage => lastPage?.after || null,
+  });
 
   const formattedData = useMemo(() => {
-    const dataFormated = data?.pages.flatMap(imageData => {
+    const formatted = data?.pages.flatMap(imageData => {
       return imageData.data.flat();
     });
-
-    return dataFormated
+    return formatted;
   }, [data]);
 
-  if(isLoading && !isError) {
-    return <Loading/>
+  if (isLoading && !isError) {
+    return <Loading />;
   }
 
-  if(isLoading && isError) {
-    return <Error/>
+  if (!isLoading && isError) {
+    return <Error />;
   }
-
 
   return (
     <>
       <Header />
-      <Box maxW={1120} px={20} mx="auto" my={20}>
+
+      <Box maxW={1120} px={[10, 15, 20]} mx="auto" my={[10, 15, 20]}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        {hasNextPage && (
+          <Button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
